06a48877bfd0156c48e5045306e55894dc2a163d
20IanHarris24 <ian_harris@ymail.com>
2024-05-28 17:42:09 +0200

    Add project files.

---
+++ Assignment3/Person.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class Person
+    {
+        private int _age;
+        private string _fName;
+        private string _lName;
+        private double _height;
+        private double _weight;
+
+
+        public Person(string firstName, string lastName, int age, double height, double weight) ///Instance Constructor
+        {
+            _fName = firstName;
+            _lName = lastName;
+            _age = age;
+            _height = height;
+            _weight = weight;
+        }
+
+        public int GetAge() 
+        {
+            return _age;
+        }
+
+        public void SetAge(int age)
+        {
+           _age = age; 
+        }
+
+
+        public string GetFName()
+        {
+            return _fName;
+        }
+
+        public void SetFName(string fName)
+        {
+            _fName = fName;
+        }
+
+        public string GetLName()
+        {
+            return _lName;
+        }
+
+        public void SetLName(string lName)
+        {
+            _lName = lName;
+        }
+
+        public double GetHeight()
+        {
+            return _height;
+        }
+
+        public void SetHeight(double height)
+        {
+            _height = height;
+        }
+
+        public double GetWeight()
+        {
+            return _weight;
+        }
+
+        public void SetWeight(double weight)
+        {
+            _weight = weight;
+        }
+
+
+
+
+    }
+    
+
+
+
+
+
+
+   
+}
---
+++ Assignment3/Program.cs
@@ -0,0 +1,28 @@
+﻿using Assignment3;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person person = new Person("Terry", "Sunport", 37, 1.75, 87.6);
+        Person person2 = new Person("Julia", "Turnpike", 31, 1.78, 80.3);
+
+        person.GetFName();
+        person.GetWeight();
+        person2.GetFName();
+        person2.GetAge();
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"\n\t First Name: {person.GetFName()} weight: {person.GetWeight()}KGs\n");
+        Console.WriteLine($"\n\t First Name: {person2.GetFName()} age: {person2.GetAge()}years\n");
+
+
+
+        Console.ResetColor();
+
+        person.SetWeight(89.5);
+        person2.SetAge(32);
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine($"\n\t First Name: {person.GetFName()} updated weight: {person.GetWeight()}KGs\n");
+        Console.WriteLine($"\n\t First Name: {person2.GetFName()} updated age: {person2.GetAge()}years\n");
+    }
+}
